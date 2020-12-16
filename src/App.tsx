071ec95a5430adca17d4ace59ab1015109cd2f8f
Nicholas King <v-nicki@microsoft.com>
2020-12-15 16:39:07 -0800

    Intro page and survey integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './survey.min.css'
+import { PageModel, QuestionRadiogroupModel, ReactSurveyModel, Survey } from 'survey-react';
+import surveyJson from './data/survey.json';
+import Intro from './Intro';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface SurveyChangedOptions {
+  name: string,
+  question: QuestionRadiogroupModel,
+  value: string
+}
+
+interface SurveyCompleteOptions {
+  showDataSaving: (text: string) => any,
+  showDataSavingError: (text: string) => any,
+  showDataSavingSuccess: (text: string) => any,
+  showDataSavingClear: () => any,
+  isCompleteOnTrigger: () => boolean
+}
+
+interface CurrentPageChangedOptions {
+  oldCurrentPage: PageModel,
+  newCurrentPage: PageModel,
+  isNextPage: boolean,
+  isPrevPage: boolean
+}
+
+function handleComplete(sender: ReactSurveyModel, options: SurveyCompleteOptions): void {
+  console.log("Complete", sender, options);
+  // Sends the survey back to the first page while keeping the response data
+  sender.clear(false);
+}
+
+function handleCurrentPageChanged(sender: ReactSurveyModel, options: CurrentPageChangedOptions): void {
+  console.log("CurrentPageChanged", sender, options);
+}
+
+function handleValueChanged(sender: ReactSurveyModel, options: SurveyChangedOptions): void {
+  console.log("ValueChanged", sender, options);
+}
+
+const App: React.FunctionComponent = () => {
+  const [showIntro, setShowIntro] = useState(true);
+  const [surveyComplete, setSurveyComplete] = useState(false);
+
+  const handleComplete = (sender: ReactSurveyModel, options: SurveyCompleteOptions): void => {
+    console.log("Complete", sender, options);
+    // Sends the survey back to the first page while keeping the response data
+    sender.clear(false);
+    setSurveyComplete(true);
+  }
+
+  if (showIntro) {
+    return (
+      <div className="App">
+        <Intro onStartClick={() => setShowIntro(false)}/>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Survey json={surveyJson} 
+          onValueChanged={handleValueChanged}
+          onCurrentPageChanged={handleCurrentPageChanged}
+          onComplete={handleComplete}/>
+      </div>
+    );
+  }
 }
 
 export default App;

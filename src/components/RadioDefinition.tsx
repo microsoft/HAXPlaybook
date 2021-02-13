344f9d77e1217c2228f0e54fefaea3114f3ef46f
Nicholas King <v-nicki@microsoft.com>
2021-02-12 16:12:44 -0800

    Independently scrolling panels (#60)
    * Independently scrolling panes. Use CSS grid layout
    
    * Scale text size and survey width
    
    * Scale down the title text a bit
    
    * Scale the survey width better

--- src/components/RadioDefinition.tsx
+++ src/components/RadioDefinition.tsx
@@ -15,7 +15,7 @@ const RadioDefinition: React.FunctionComponent<RadioDefinitionProps> = ({ name, 
   const [showHelp, setShowHelp] = useState(false);
 
   return (
-    <div style={{ marginLeft: "calc(1.75em + 3px)", marginTop: "-0.8em", lineHeight: "1.4em" }}>
+    <div className="radio-definition">
       <div className="light-text" style={{ display: "inline", fontSize: "0.85em", marginRight: "0.75em" }} dangerouslySetInnerHTML={{ __html: definition }}></div>
       <Help name={name} examples={examples} show={showHelp} onClose={() => setShowHelp(false)} />
       <BsFillQuestionCircleFill color="#00ECF2" style={{ cursor: 'pointer', marginTop: "-3px" }} onClick={() => setShowHelp(true)} />
